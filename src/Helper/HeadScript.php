adb2756d65d2ca1ce483250e00e0abbfd10ad2be
Mat <mberchtold@gmail.com>
2015-12-02 13:09:34 +0100

    added check for xhtml

--- src/Helper/HeadScript.php
+++ src/Helper/HeadScript.php
@@ -400,7 +400,7 @@ class HeadScript extends Placeholder\Container\AbstractStandalone
         $addScriptEscape = !(isset($item->attributes['noescape'])
             && filter_var($item->attributes['noescape'], FILTER_VALIDATE_BOOLEAN));
 
-        if ($item->type != '') {
+        if ($item->type != '' || $this->view->plugin('doctype')->isXhtml()) {
             $type = ($this->autoEscape) ? $this->escape($item->type) : $item->type;
             $html = '<script type="' . $type . '"' . $attrString . '>';
         } else {
