aee27ad740f97bf465e169aededa2da04525b8fb
webimpress <contact@webimpress.com>
2016-06-21 23:19:12 +0100

    reduced complexity of `sprintf`

--- bin/templatemap_generator.php
+++ bin/templatemap_generator.php
@@ -121,13 +121,12 @@ $entries = array_map(function ($file) use ($basePath, $realPath) {
         : $template;
 
     $template = preg_replace('#^\.*/#', '', $template);
-    $file     = sprintf('__DIR__ . \'/%s\'', $file);
 
-    return sprintf("    '%s' => %s,\n", $template, $file);
+    return sprintf("    '%s' => __DIR__ . '/%s',", $template, $file);
 }, $files);
 
 echo '<' . "?php\nreturn [\n"
-    . implode('', $entries)
+    . implode("\n", $entries) . "\n"
     . '];';
 
 exit(0);
