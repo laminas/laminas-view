d83fad5b8bcfec6165354f6964e5b9ee126dac33
Wanderson <wandersonwhcr@gmail.com>
2017-09-16 14:28:11 -0300

    Alias deprecated errors for PHPUnit lowest dep

--- test/autoload.php
+++ test/autoload.php
@@ -4,3 +4,8 @@
  * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-view/blob/master/LICENSE.md New BSD License
  */
+
+if (class_exists(PHPUnit_Framework_Error_Deprecated::class)
+    && ! class_exists(PHPUnit\Framework\Error\Deprecated::class)) {
+    class_alias(PHPUnit_Framework_Error_Deprecated::class, PHPUnit\Framework\Error\Deprecated::class);
+}
