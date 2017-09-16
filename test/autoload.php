db6b1c30ed8cc28ddae7281b657b3263c09eeb67
Wanderson <wandersonwhcr@gmail.com>
2017-09-16 14:26:01 -0300

    Create an autoload script for tests

---
+++ test/autoload.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-view for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-view/blob/master/LICENSE.md New BSD License
+ */
