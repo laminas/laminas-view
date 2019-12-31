27af8024c78f4a57101925647c69a439dd62dd82
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 12:03:46 -0600

    Rewriting as Laminas Project package

--- src/Helper/Navigation/PluginManager.php
+++ src/Helper/Navigation/PluginManager.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-view for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-view/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-view/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\View\Helper\Navigation;
+namespace Laminas\View\Helper\Navigation;
 
-use Zend\View\Exception;
-use Zend\View\HelperPluginManager;
+use Laminas\View\Exception;
+use Laminas\View\HelperPluginManager;
 
 /**
  * Plugin manager implementation for navigation helpers
@@ -27,10 +26,10 @@ class PluginManager extends HelperPluginManager
      * @var array
      */
     protected $invokableClasses = array(
-        'breadcrumbs' => 'Zend\View\Helper\Navigation\Breadcrumbs',
-        'links'       => 'Zend\View\Helper\Navigation\Links',
-        'menu'        => 'Zend\View\Helper\Navigation\Menu',
-        'sitemap'     => 'Zend\View\Helper\Navigation\Sitemap',
+        'breadcrumbs' => 'Laminas\View\Helper\Navigation\Breadcrumbs',
+        'links'       => 'Laminas\View\Helper\Navigation\Links',
+        'menu'        => 'Laminas\View\Helper\Navigation\Menu',
+        'sitemap'     => 'Laminas\View\Helper\Navigation\Sitemap',
     );
 
     /**
