390087c81f98dc2f1ca8f4a07b6e9ff7f227e738
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-01-28 12:23:33 -0600

    Rewrite references to Zend to instead reference Laminas

--- test/Helper/PartialTest.php
+++ test/Helper/PartialTest.php
@@ -6,18 +6,18 @@
  * @license   https://github.com/laminas/laminas-view/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\View\Helper;
+namespace LaminasTest\View\Helper;
 
 use PHPUnit_Framework_TestCase as TestCase;
-use Zend\View\Helper\Partial;
-use Zend\View\Model\ViewModel;
-use Zend\View\Renderer\PhpRenderer as View;
+use Laminas\View\Helper\Partial;
+use Laminas\View\Model\ViewModel;
+use Laminas\View\Renderer\PhpRenderer as View;
 
 /**
  * Test class for Partial view helper.
  *
- * @group      Zend_View
- * @group      Zend_View_Helper
+ * @group      Laminas_View
+ * @group      Laminas_View_Helper
  */
 class PartialTest extends TestCase
 {
