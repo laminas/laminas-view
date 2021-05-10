93f98b80b08a91e3d6458c7f81e66b26bfdeb919
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:10:58 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/autoload.php
+++ test/autoload.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-view for the canonical source repository
- * @copyright https://github.com/laminas/laminas-view/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-view/blob/master/LICENSE.md New BSD License
- */
-
 if (class_exists(PHPUnit_Framework_Error_Deprecated::class)
     && ! class_exists(PHPUnit\Framework\Error\Deprecated::class)) {
     class_alias(PHPUnit_Framework_Error_Deprecated::class, PHPUnit\Framework\Error\Deprecated::class);
