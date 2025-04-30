dc4491bbcb228f54a00ca53ad84a36e7989c3d55
George Steel <george@net-glue.co.uk>
2025-05-01 00:09:20 +0100

    Amend the resolver interface to return the template file or throw an exception
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Resolver/ResolverInterface.php
+++ src/Resolver/ResolverInterface.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace Laminas\View\Resolver;
 
-use Laminas\View\Renderer\RendererInterface as Renderer;
-
 interface ResolverInterface
 {
     /**
      * Resolve a template/pattern name to a resource the renderer can consume
      *
-     * In version 3.0 of this component, this method will guarantee a string return type or an exception will be thrown.
-     *
-     * @param  string $name
-     * @return string|false
+     * @param non-empty-string $name
+     * @return non-empty-string
+     * @throws TemplateCannotBeFound If the given template cannot be found/resolved.
      */
-    public function resolve($name, ?Renderer $renderer = null);
+    public function resolve(string $name): string;
 }
