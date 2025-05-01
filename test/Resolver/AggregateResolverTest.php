def7d3ef00964424d6140ce381486d683b820a25
George Steel <george@net-glue.co.uk>
2025-05-01 12:45:15 +0100

    Refactor `AggregateResolver`
    Adds types and the final keyword.
    
    Brings behaviour in-line with the interface definition.
    
    Removes `FAILURE_*` constants
    
    Removes deprecated methods
    
    - `getLastSuccessfulResolver`
    - `getLastLookupFailure`
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/Resolver/AggregateResolverTest.php
+++ test/Resolver/AggregateResolverTest.php
@@ -4,106 +4,63 @@ declare(strict_types=1);
 
 namespace LaminasTest\View\Resolver;
 
-use Exception;
-use Laminas\View\Resolver;
+use Laminas\View\Resolver\AggregateResolver;
+use Laminas\View\Resolver\TemplateCannotBeFound;
+use Laminas\View\Resolver\TemplateMapResolver;
 use PHPUnit\Framework\TestCase;
 
 final class AggregateResolverTest extends TestCase
 {
     public function testAggregateIsEmptyByDefault(): void
     {
-        $resolver = new Resolver\AggregateResolver();
+        $resolver = new AggregateResolver();
         $this->assertCount(0, $resolver);
     }
 
     public function testCanAttachResolvers(): void
     {
-        $resolver = new Resolver\AggregateResolver();
-        $resolver->attach(new Resolver\TemplateMapResolver());
+        $resolver = new AggregateResolver();
+        $resolver->attach(new TemplateMapResolver());
         $this->assertCount(1, $resolver);
-        $resolver->attach(new Resolver\TemplateMapResolver());
+        $resolver->attach(new TemplateMapResolver());
         $this->assertCount(2, $resolver);
+
+        self::assertContainsOnlyInstancesOf(TemplateMapResolver::class, $resolver);
     }
 
     public function testReturnsNonFalseValueWhenAtLeastOneResolverSucceeds(): void
     {
-        $resolver = new Resolver\AggregateResolver();
-        $resolver->attach(new Resolver\TemplateMapResolver([
+        $resolver = new AggregateResolver();
+        $resolver->attach(new TemplateMapResolver([
             'foo' => 'bar',
         ]));
-        $resolver->attach(new Resolver\TemplateMapResolver([
+        $resolver->attach(new TemplateMapResolver([
             'bar' => 'baz',
         ]));
         $test = $resolver->resolve('bar');
         $this->assertEquals('baz', $test);
     }
 
-    public function testLastSuccessfulResolverIsNullInitially(): void
+    public function testExceptionThrownWhenNoResolverSucceeds(): void
     {
-        $resolver = new Resolver\AggregateResolver();
-        $this->assertNull($resolver->getLastSuccessfulResolver());
-    }
-
-    public function testCanAccessResolverThatLastSucceeded(): void
-    {
-        $resolver    = new Resolver\AggregateResolver();
-        $fooResolver = new Resolver\TemplateMapResolver([
-            'foo' => 'bar',
-        ]);
-        $barResolver = new Resolver\TemplateMapResolver([
-            'bar' => 'baz',
-        ]);
-        $bazResolver = new Resolver\TemplateMapResolver([
-            'baz' => 'bat',
-        ]);
-        $resolver->attach($fooResolver)
-                 ->attach($barResolver)
-                 ->attach($bazResolver);
-
-        $test = $resolver->resolve('bar');
-        $this->assertEquals('baz', $test);
-        $this->assertSame($barResolver, $resolver->getLastSuccessfulResolver());
-    }
-
-    public function testReturnsFalseWhenNoResolverSucceeds(): void
-    {
-        $resolver = new Resolver\AggregateResolver();
-        $resolver->attach(new Resolver\TemplateMapResolver([
+        $resolver = new AggregateResolver();
+        $resolver->attach(new TemplateMapResolver([
             'foo' => 'bar',
         ]));
-        $this->assertFalse($resolver->resolve('bar'));
-        $this->assertEquals(Resolver\AggregateResolver::FAILURE_NOT_FOUND, $resolver->getLastLookupFailure());
-    }
-
-    public function testLastSuccessfulResolverIsNullWhenNoResolverSucceeds(): void
-    {
-        $resolver    = new Resolver\AggregateResolver();
-        $fooResolver = new Resolver\TemplateMapResolver([
-            'foo' => 'bar',
-        ]);
-        $resolver->attach($fooResolver);
-        $resolver->resolve('foo');
-        $this->assertSame($fooResolver, $resolver->getLastSuccessfulResolver());
-
-        try {
-            $resolver->resolve('bar');
-            $this->fail('Should not have resolved!');
-        } catch (Exception $e) {
-            // exception is expected
-        }
-        $this->assertNull($resolver->getLastSuccessfulResolver());
+        $this->expectException(TemplateCannotBeFound::class);
+        $resolver->resolve('bar');
     }
 
     public function testResolvesInOrderOfPriorityProvided(): void
     {
-        $resolver    = new Resolver\AggregateResolver();
-        $fooResolver = new Resolver\TemplateMapResolver([
+        $resolver    = new AggregateResolver();
+        $fooResolver = new TemplateMapResolver([
             'bar' => 'foo',
         ]);
-        $barResolver = new Resolver\TemplateMapResolver([
+        $barResolver = new TemplateMapResolver([
             'bar' => 'bar',
         ]);
-        $bazResolver = new Resolver\TemplateMapResolver([
+        $bazResolver = new TemplateMapResolver([
             'bar' => 'baz',
         ]);
         $resolver->attach($fooResolver, -1)
@@ -114,10 +71,44 @@ final class AggregateResolverTest extends TestCase
         $this->assertEquals('bar', $test);
     }
 
-    public function testReturnsFalseWhenAttemptingToResolveWhenNoResolversAreAttached(): void
+    public function testExceptionThrownWhenAttemptingToResolveWhenNoResolversAreAttached(): void
     {
-        $resolver = new Resolver\AggregateResolver();
-        $this->assertFalse($resolver->resolve('foo'));
-        $this->assertEquals(Resolver\AggregateResolver::FAILURE_NO_RESOLVERS, $resolver->getLastLookupFailure());
+        $resolver = new AggregateResolver();
+        $this->expectException(TemplateCannotBeFound::class);
+        $resolver->resolve('foo');
+    }
+
+    public function testResolversCanBeSuppliedInTheConstructor(): void
+    {
+        $fooResolver = new TemplateMapResolver([
+            'bar' => 'foo',
+        ]);
+        $barResolver = new TemplateMapResolver([
+            'bar' => 'bar',
+        ]);
+
+        $resolver = new AggregateResolver([
+            $barResolver,
+            $fooResolver,
+        ]);
+
+        self::assertSame('bar', $resolver->resolve('bar'));
+    }
+
+    public function testConstructorResolversAreResolvedInFIFOOrder(): void
+    {
+        $a = new TemplateMapResolver([
+            'bar' => '1',
+        ]);
+        $b = new TemplateMapResolver([
+            'bar' => '2',
+        ]);
+        $c = new TemplateMapResolver([
+            'bar' => '3',
+        ]);
+
+        $resolver = new AggregateResolver([$c, $b, $a]);
+
+        self::assertSame('3', $resolver->resolve('bar'));
     }
 }
