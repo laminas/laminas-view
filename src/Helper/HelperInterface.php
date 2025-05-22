b1f2261d2c242f6f8f4ff4136b4b762663cdf35a
George Steel <george@net-glue.co.uk>
2025-05-22 22:25:05 +0100

    `ServerUrl` and `HelperInterface` Deprecations
    See #322
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Helper/HelperInterface.php
+++ src/Helper/HelperInterface.php
@@ -11,12 +11,18 @@ interface HelperInterface
     /**
      * Set the View object
      *
+     * @deprecated Since 2.40.0 This method will be removed in 3.0 without replacement. Dependency injection should be
+     *             used to provide dependencies to consuming classes.
+     *
      * @return HelperInterface
      */
     public function setView(Renderer $view);
 
     /**
      * Get the View object
+     *
+     * @deprecated Since 2.40.0 This method will be removed in 3.0 without replacement. Accessing dependencies in this
+     *             way breaks encapsulation and is unnecessary when DI is used effectively.
      *
      * @return Renderer|null
      */
